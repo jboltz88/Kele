8e03ad80583a8420f64f8503b06757a70b4f59e7
Justin Boltz <boltz.justin@gmail.com>
2016-09-16 17:55:19 -0500

    Fix module error

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,16 @@
 require "kele/version"
+require "httparty"
 
-module Kele
-  # Your code goes here...
+class Kele
+  include HTTParty
+
+  def initialize(email, password)
+    @api_url = "https://www.bloc.io/api/v1"
+    response = self.class.post("#{@api_url}/sessions", body: {email: email, password: password})
+    raise "invalid email/pass" if response.code != 200
+    #puts response.to_hash.inspect
+    @auth_token = response["auth_token"]
+  end
+
+  # url = "#{@api_url}/users/me"
 end
